52a46ec019479d9fc7df02e268ee3bbd8391b8b2
castlj1 <CASTLJ1@nationwide.com>
2017-09-12 21:21:46 -0400

    Fixing scanning in init.java

--- inventory-api/src/main/java/com/swayzetrain/inventory/api/Init.java
+++ inventory-api/src/main/java/com/swayzetrain/inventory/api/Init.java
@@ -2,14 +2,18 @@ package com.swayzetrain.inventory.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @SpringBootApplication
 @ComponentScan({"com.swayzetrain.inventory"})
+@EntityScan("com.swayzetrain.inventory")
+@EnableJpaRepositories("com.swayzetrain.inventory")
 public class Init extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
